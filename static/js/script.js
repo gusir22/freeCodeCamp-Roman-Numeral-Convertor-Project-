704eff700805a592301d8b1f0f5e3f9e00c61728
Gustavo Rivero <gusir22@gmail.com>
2025-04-21 16:09:46 -0400

    Validated for empty input value

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,9 @@
-console.log("Roman Numerals!");
+function convertToRoman(form) {
+    let number = form.number.value;
+
+    if (number) {
+        console.log(number);
+    } else {
+        document.getElementById('output').innerText = "Please enter a valid number";
+    }
+}
