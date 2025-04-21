1d156312c98fab8ab05e42e9dd48940daab9a9be
Gustavo Rivero <gusir22@gmail.com>
2025-04-21 16:22:38 -0400

    Updated validation to check for negative numbers

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,15 @@
 function convertToRoman(form) {
     let number = form.number.value;
+    let outputMessage; // init empty string
 
-    if (number) {
-        console.log(number);
+    if (number == '') {
+        outputMessage = "Please enter a valid number";
+    } else if (number < 1) {
+        outputMessage = "Please enter a number greater than or equal to 1";
     } else {
-        document.getElementById('output').innerText = "Please enter a valid number";
+        console.log(number);
+        outputMessage = `${number}`;
     }
+
+    document.getElementById('output').innerText = outputMessage;
 }
